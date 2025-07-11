e67b49fb547c70943d078103148e9f1652521b1f
mateus gabriel barbosa <mateus_gabriel96@outlook.com>
2025-07-10 22:35:26 -0300

    feat: create routes: create question and room, get room questions

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,6 +1,8 @@
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
-import { db } from "../db/connection.ts";
-import { schema } from "../db/schema/index.ts";
+import { db } from "../../db/connection.ts";
+import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm";
+import { create } from "domain";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get("/rooms", async () => {
@@ -8,8 +10,12 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        questionsCount: count(schema.questions.id),
+        createdAt: schema.rooms.createdAt,
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt);
 
     return result;
